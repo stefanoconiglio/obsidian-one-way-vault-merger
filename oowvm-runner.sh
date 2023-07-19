accd967f0bdc9b8ff2fdcd54d50be06852b80735
Stefano Coniglio <33290924+stefanoconiglio@users.noreply.github.com>
2023-07-19 17:26:44 +0200

    Add files via upload

--- oowvm-runner.sh
+++ oowvm-runner.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# old oneliner
-# rm -r __MACOSX; rm -r iphone PC_backup; unzip iphone; cp -r PC PC_backup
+rm -rf __MACOSX
 
-rm -r __MACOSX
+folderA="iphone"
+folderB="PC"
+
+echo "This script is about to merge the content of $folderA.zip into your main Obsidian vault $folderB. If this is NOT what you want to do, smash Ctrl+C now!"
+read
 
 echo "Close Obsidian and press a key when done"
 read
 
-echo "Press a key to run: rm -r iphone"
+echo "Press a key to run: rm -r $folderA"
 read
-rm -r iphone
+rm -rf ${folderA}
 
-echo "Press a key to run: unzip iphone.zip"
+echo "Press a key to run: unzip $folderA.zip"
 read
-unzip iphone.zip
+unzip ${folderA}.zip
 
 timestamp=`date +%s`
-echo "Press a key to run: cp -r PC PC_bkp_${timestamp}"
+echo "Press a key to run: cp -r ${folderB} ${folderB}_bkp_${timestamp}"
 read
-cp -r PC PC_bkp_${timestamp}
+cp -r ${folderB} ${folderB}_bkp_${timestamp}
 
-echo "Press a key to run: oowvm iphone PC | less"
+echo "Press a key to run: oowvm $folderA $folderB | less"
 read
-oowvm iphone PC | less
+oowvm ${folderA} ${folderB} | less
